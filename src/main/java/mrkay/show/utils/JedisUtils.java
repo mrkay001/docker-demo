bdd245004c0672963f900e0b089b3ae22f611142
liukai <kai.liu@keepsoft.net>
2022-03-03 13:44:10 +0800

    redis引入

---
+++ src/main/java/mrkay/show/utils/JedisUtils.java
@@ -0,0 +1,14 @@
+package mrkay.show.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * redis操作工具类
+ *
+ * @author liukai
+ * @date 2022/3/3 13:41
+ */
+@Component
+public class JedisUtils {
+
+}
